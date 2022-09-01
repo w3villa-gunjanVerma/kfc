7b6eb435ad882744146f4fe8adf1bc15863e5c31
Gunjan Verma <gunjan.verma@w3villa.com>
2022-09-01 17:36:48 +0530

    added js file

---
+++ assets/js/carousel.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function () {
+// Hero section
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 3
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+//food list
+$('.owl-carousel2').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 7
+        }
+    }
+});
+//feedback
+$('.owl-carousel3').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+});
+
